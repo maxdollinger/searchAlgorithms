6f27ad9f598ae850dd9dc02d5b7a1aa4b3b21a22
Maximilian Dollinger <70093586+maxdollinger@users.noreply.github.com>
2020-12-15 13:30:38 +0100

    Update sorting.test.js

--- sorting.test.js
+++ sorting.test.js
@@ -2,7 +2,7 @@ const {input, expected} = require('./createSortedAndShuffledArr')();
 const { mergeSort } = require('./sortingAlgorithms');
 
 //Simple unit-test useing jest
-test('Array sorting functions', () => {
+test('expects an ascending sorted array', () => {
     const output = mergeSort(input);
     expect(output.toString()).toBe(expected.toString());
 })
